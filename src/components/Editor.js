e3f08aefd7d58d865a7435b9786451c8906028f9
MarioSimou <marios.simouu@gmail.com>
2019-09-30 21:32:31 +0100

    first-commit

---
+++ src/components/Editor.js
@@ -0,0 +1,20 @@
+import React from 'react-dom'
+import { makeStyles } from '@material-ui/styles'
+
+const Editor = props => {
+  const classes = useStyles()
+  
+  return (
+    <div>
+      Editor
+    </div>
+  )
+}
+
+const useStyles = makeStyles(theme => ({
+  root: {
+
+  }
+}))
+
+export default Editor
---
+++ src/components/Sidebar.js
@@ -0,0 +1,19 @@
+import React from 'react-dom'
+import { makeStyles } from '@material-ui/styles'
+
+const Sidebar = props => {
+  const classes = useStyles()
+  return (
+    <div>
+      sidebar
+    </div>
+  )
+}
+
+const useStyles = makeStyles(theme => ({
+  root: {
+
+  }
+}))
+
+export default Sidebar
