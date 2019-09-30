80691dbeef56d1faa8dcc9a4673741255990cff1
MarioSimou <marios.simouu@gmail.com>
2019-09-30 22:10:39 +0100

    adding config for firestore

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,12 @@
 import React from 'react'
-
+import firebase from 'firebase'
 const App = props => {
+
+  React.useEffect(() => {
+    firebase.firestore().collection('tasks').get().then(snapshot => {
+      console.log(snapshot.docs.map(doc => ({ id:doc.id, ...doc.data()})))
+    })
+  }, [])
   return (
     <div>
       hello world
